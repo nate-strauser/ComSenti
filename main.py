16cb5e16a5f52e68a1c18bac41b7d12cbe82461b
Nathan Strauser <nate@minnow.(none)>
2010-09-08 19:01:44 -0400

    test change 1

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from google.appengine.ext.webapp import util
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('ComSenti coming soon')
+        self.response.out.write('ComSenti coming sooner')
 
 
 def main():
