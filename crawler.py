9b4663ea4bb9d627a4a1ecb134b96f43c597be66
Aaron Campbell <tehjakey@gmail.com>
2010-09-18 12:16:49 -0400

    - Searching using lang=en - Searching with the max page size of 100 results - Added a start date and end date range - Added paging functionality to pull data within the date range until all results are returned. We currently exit the loop after one iteration to limit the data.

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import webapp
 from twitter_search import TwitterSearch
 from google.appengine.ext.webapp import util
+from datetime import datetime
 from sentiment import SentimentAnalyzer
 from models import *
 import random
@@ -8,23 +9,40 @@ import datetime
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Crawler executing twitter search for \' turtle \' ')
+        TWITTER_DATETIME_PATTERN = '%a, %d %b %Y %H:%M:%S +0000'
         twitter_search = TwitterSearch()
-        search_results = twitter_search.execute_search("turtle", 10)
-        if search_results is None:
-            self.response.out.write('Crawler found zero results')
-        else:
-            self.response.out.write('Crawler found the following results')
-            self.response.out.write(search_results)
-            #fake for now
-            query = Company.all()
-            sentiment_analyzer = SentimentAnalyzer()
-            for company in query:
-            	source = "http://www.twitter.com/tweet/" + str(random.randint(1, 1000000))
-            	text = "fake tweet text"
-            	date = datetime.datetime.now()
-            	sentiment_analyzer.analyze_and_track(company, source, text, date)
+        sentiment_analyzer = SentimentAnalyzer()
+        
+        query = Company.all()
+        for company in query:
+            self.response.out.write('<h1>Crawler executing twitter search for ' + company.name + '</h1>')
+            search_results = twitter_search.execute_search(company.name, '2010-09-14', '2010-09-15')
+            if search_results is None:
+                self.response.out.write('<h3>Crawler found zero results</h3>')
+            else:
+                self.response.out.write('<h3>Crawler found the following [%s] results</h3>' % len(search_results))
+                self.response.out.write('<table border="0"><tr>')
+                for tweet in search_results:
+                    self.response.out.write('<td><img src="' + tweet['profile_image_url'] + '"></td>')
+                    self.response.out.write('<td>' + tweet['from_user'] + '</td>')
+                    self.response.out.write('<td>' + tweet['text'] + '</td')
+                    self.response.out.write('</tr><tr>')
+                    
+                    tweet_date = datetime.datetime.strptime(tweet['created_at'], TWITTER_DATETIME_PATTERN)
+                    #un-fake/uncomment to start calling the analyzer with live tweet data. For now we just send to the
+                    #  browser for convenience.
+                    #sentiment_analyzer.analyze_and_track(company, tweet['source'], tweet['text'], tweet_date)
+                    
+                    #TODO: add tweet['profile_image_url'], tweet['from_user'] to the sentiment
+                    #TODO: add tweet['id'] to the sentiment to prevent duplicate sentiment from being added to the db.
+                    #  This would not be needed if the crawler runs only once a day to gather yesterday's tweets.
+            self.response.out.write('</tr></table>')
             
+            #fake for now
+            source = "http://www.twitter.com/tweet/" + str(random.randint(1, 1000000))
+            text = "fake tweet text"
+            date = datetime.datetime.now()
+            sentiment_analyzer.analyze_and_track(company, source, text, date)
 
 def main():
     application = webapp.WSGIApplication([('/crawler/', MainHandler)],
--- twitter_search.py
+++ twitter_search.py
@@ -8,31 +8,64 @@ from google.appengine.api import urlfetch
 class TwitterSearch:
     TWITTER_SEARCH_URL_BASE = 'http://search.twitter.com/search.json'
 
-    def execute_search(self, query_text, result_count):
-        twitter_search_url = "%s?%s" % (self.TWITTER_SEARCH_URL_BASE, self.create_query_string(query_text, result_count))
+    ##
+    # Searches twitter for a search term from a start date to an end date. Multiple requests are made to the 
+    #    twitter search api until all results are retured. 
+    #
+    #    @param search_term:  The search term (string).
+    #    @param start_date: The start date in yyyy-mm-dd format (string).
+    #    @param end_date: The end date in yyyy-mm-dd format (string).
+    #    @return: List of json search results.
+    def execute_search(self, search_term, start_date, end_date):
         search_results = []
-        twitter_response_json = self.fetch(twitter_search_url)
-        if not twitter_response_json:
-            log.info("No results")
-            return None
-        search_results.extend(twitter_response_json.get('results'))
+        twitter_search_url = "%s?%s" % (self.TWITTER_SEARCH_URL_BASE, self.create_query_string(search_term, start_date, end_date))
+        while(True):
+            twitter_response_json = self.fetch(twitter_search_url)
+            if not twitter_response_json:
+                log.info("No results")
+                return None
+            search_results.extend(twitter_response_json.get('results'))
+            if twitter_response_json.get('next_page'):
+                twitter_search_url = "%s%s" % (self.TWITTER_SEARCH_URL_BASE, twitter_response_json.get('next_page'))
+                #TODO: remove this break when we are ready to pull pages of results 
+                #temporarily only run one loop
+                break
+            elif twitter_response_json.get('max_id') == -1:
+                log.error("search response max_id was -1")
+                return None
+                break
+            else:
+                log.debug("search finished without errors.")
+                break
         return search_results
 
-    def create_query_string(self, search_string, result_count):
-        query_string = {'q': ('%s' % (search_string)), 'rpp': result_count};
+    ##
+    # Creates the query string for the twitter search URL. The max limit of 100 results is used.
+    #    The language is set for english.
+    #
+    #    @param search_term: The search term.
+    #    @param start_date: The start date in yyyy-mm-dd format (string).
+    #    @param end_date: The end date in yyyy-mm-dd format (string).
+    #    @return: The encoded search_string.
+    #
+    #    @see http://search.twitter.com/api/
+    def create_query_string(self, search_term, start_date, end_date):        
+        query_string = {'lang': 'en', 'q': search_term, 'since': start_date, 'until': end_date ,'rpp': 100};
         return urllib.urlencode(query_string)
 
-    def fetch(self, url):
-        log.info("Executing %s", url)
+    ##
+    # Makes a synchronous request to the twitter search URL.
+    #    @param twitter_search_url: The url to fetch.
+    #    @see: http://code.google.com/appengine/docs/python/urlfetch/fetchfunction.html
+    def fetch(self, twitter_search_url):
+        log.info("Executing %s", twitter_search_url)
         try:
-            twitter_response = urlfetch.fetch(url)
+            twitter_response = urlfetch.fetch(twitter_search_url)
             if twitter_response.status_code == 200:
                 log.info("Twitter response [%s]", (twitter_response.content)[0:25])
                 return json.loads(twitter_response.content)
             else:
                 log.error("Http status code [%s] , response [%s]", twitter_response.status_code, twitter_response)
         except urlfetch.Error, e:
-            log.error("Could not fetch url [%s] [%s]", url, e)
+            log.error("Could not fetch twitter_search_url [%s] [%s]", twitter_search_url, e)
             return None
-    
-
